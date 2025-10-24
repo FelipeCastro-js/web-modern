479746212685b8c70981c3a4e485922080b659b5
Felipe Castro <carloscastro1860@gmail.com>
2025-10-24 11:54:03 -0500

    style: add animation with motion and design to component feature

---
+++ src/components/Features/Features.data.ts
@@ -0,0 +1,20 @@
+export const dataFeatures = [
+  {
+    id: 1,
+    image: "medal",
+    text: "Approved by other customers",
+    additionalClass: "mt-0",
+  },
+  {
+    id: 2,
+    image: "construction",
+    text: "Working for 20 years",
+    additionalClass: "mt-20",
+  },
+  {
+    id: 3,
+    image: "calendar",
+    text: "Automatic calendar platform",
+    additionalClass: "mt-0",
+  },
+];
---
+++ src/components/Features/Features.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import { MotionTransition } from "../MotionTransition";
+import { dataFeatures } from "./Features.data";
+
+export function Features() {
+  return (
+    <div
+      id="features"
+      className="grid grid-cols-2 gap-16 my-20 md:grid-cols-4 md:py-56"
+    >
+      <div>
+        <h2 className="mb-4 text-4xl font-bold">
+          Because we are <br />
+          <span className="text-emerald-500">better than others</span>
+        </h2>
+      </div>
+      {dataFeatures.map(({ id, image, text, additionalClass }) => (
+        <MotionTransition key={id}>
+          <div key={id}>
+            <div
+              className={`rounded-3xl shadow-lg p-6 shadow-emerald-100 ${additionalClass}`}
+            >
+              <Image
+                src={`/assets/${image}.png`}
+                alt={`Image ${text}`}
+                width="100"
+                height="100"
+              />
+              <h3 className="mt-3 mr-20 text-2xl font-bold">{text}</h3>
+            </div>
+          </div>
+        </MotionTransition>
+      ))}
+    </div>
+  );
+}
